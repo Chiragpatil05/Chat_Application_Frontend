0e2610983c5bc7458dadf5b0a0f7d4694d6ed953
Chiragpatil05 <itschiragpatil5@gmail.com>
2024-08-02 21:33:47 +0530

    style component changes

--- src/components/styles/StyledComponents.jsx
+++ src/components/styles/StyledComponents.jsx
@@ -6,7 +6,7 @@ import { grayColor } from "../../constants/color";
 const VisuallyHiddenInput = styled("input")({
     border: 0,
     clip: "rect(0 0 0 0)",
-    height: 2,
+    height: 1,
     margin: -1,
     overflow: "hidden",
     padding: 0,
